7fa6fe57bf93a5ee7fcd18ac4c4f22e4b63aa5a6
IqraMomin <iqra2018momin@gmail.com>
2026-04-03 17:58:46 +0530

    product controller

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const getProduct = (req,res)=>{
+    res.send("Fetching all products");
+}
+
+const addProduct = (req,res)=>{
+    res.send("Adding a new product");
+}
+
+const getProductById = (req,res)=>{
+    const id = req.params.id;
+    res.send(`Fetching product with ID: ${id}`)
+}
+
+module.exports = {
+    getProduct,addProduct,getProductById
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/products",(req,res)=>{
-    res.send("Fetching all products");
-})
+const productController = require("../controllers/productController");
 
-router.post("/products",(req,res)=>{
-    res.send("Adding a new product");
-})
+router.get("/products",productController.getProduct);
 
-router.get("/products/:id",(req,res)=>{
-    const id = req.params.id;
-    res.send(`Fetching product with ID: ${id}`)
-});
+router.post("/products",productController.addProduct);
+
+router.get("/products/:id",productController.getProductById);
 
 module.exports = router
